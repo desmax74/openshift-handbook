86b0864b791cac8cddf084ce48c1819a4f3c9130
Max <desmax74@yahoo.it>
2019-03-17 18:03:43 +0100

    changed kafka's templates

--- scripts/kafka/fedora/minishift_start.sh
+++ scripts/kafka/fedora/minishift_start.sh
@@ -5,8 +5,8 @@
 # in that directory
 
 minishift profile set kafka
-minishift config set memory 8192
-minishift config set cpus 2
+minishift config set memory 10GB
+minishift config set cpus 4
 minishift config set vm-driver kvm
 
 minishift config set openshift-version v3.11.0
